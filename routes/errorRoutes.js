68557b878a14439d9952dd584f5cddf17a340ebf
Yuta <yutawatanabe@YWnoMacBook-Pro.local>
2021-03-10 09:39:50 +0900

    入門Node.js_2021/0310

---
+++ routes/errorRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router(),
+  errorController = require("../controllers/errorController");
+
+router.use(errorController.logErrors);
+router.use(errorController.respondNoResourceFound);
+router.use(errorController.respondInternalError);
+
+module.exports = router;
---
+++ routes/homeRoutes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router(),
+  homeController = require("../controllers/homeController");
+
+router.get("/", homeController.index);
+router.get("/contact", homeController.contact);
+
+module.exports = router;
